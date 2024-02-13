46916c836046cb1aa7e4870474c5b684d8eeb7bb
saidan-hasbianto <saidan.hasbianto@gmail.com>
2024-02-13 11:58:19 +0700

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mtsPortal2024',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
